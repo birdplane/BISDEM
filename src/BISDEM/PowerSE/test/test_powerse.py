474c0e645c21262fc413fa5c6b3c183ad4171a0c
btelgen <btelgen@posteo.de>
2015-03-06 10:18:40 +0100

    restructuring

---
+++ src/BISDEM/PowerSE/test/test_powerse.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+
+class PowerSETestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    # add some tests here...
+    
+    #def test_Birdplane(self):
+        #pass
+        
+if __name__ == "__main__":
+    unittest.main()
+    
