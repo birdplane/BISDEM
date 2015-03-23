Corrected sign error in rotation matrices

src/BISDEM/lib/geo.py
@@ -31,38 +31,38 @@ def triangle(A,B,a,b,c):
     
     return C1, C2
 
-def rotx(theta,x1):
+def rotx(theta,x):
     
     """
-    Rotate vector x1 around x axis with angle theta    
+    Rotate vector x around x axis with angle theta    
     """
     
     R_x = np.array([[1.,    0.,             0.],
                     [0.,    np.cos(theta),  np.sin(theta)],
                     [0.,    -np.sin(theta), np.cos(theta)]])
     
-    return np.dot(R_x,x1)
+    return np.dot(R_x,x)
 
-def roty(theta,x1):
+def roty(theta,x):
     
     """
-    Rotate vector x1 around y axis with angle theta  
+    Rotate vector x around y axis with angle theta  
     """
        
-    R_y = np.array([[np.cos(theta),     0.,     np.sin(theta)],
+    R_y = np.array([[np.cos(theta),     0.,     -np.sin(theta)],
                     [0.,                1.,     0.],
-                    [-np.sin(theta),    0.,     np.cos(theta)]])
+                    [np.sin(theta),    0.,     np.cos(theta)]])
     
-    return np.dot(R_y,x1)
+    return np.dot(R_y,x)
 
-def rotz(theta,x1):
+def rotz(theta,x):
     
     """
-    Rotate vector x1 around z axis with angle theta         
+    Rotate vector x around z axis with angle theta         
     """
     
-    R_z = np.array([[np.cos(theta),     -np.sin(theta),     0.],
-                    [np.sin(theta),     np.cos(theta),      0.],
+    R_z = np.array([[np.cos(theta),     np.sin(theta),     0.],
+                    [-np.sin(theta),     np.cos(theta),      0.],
                     [0.,                0.,                 1.]])
     
-    return np.dot(R_z,x1)
+    return np.dot(R_z,x)